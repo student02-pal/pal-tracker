davwards/pal-tracker-again

Add TimeEntry MVC in memory

src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -1,0 +1,86 @@
+package io.pivotal.pal.tracker;
+
+public class TimeEntry {
+    private long id;
+    private long projectId;
+    private long userId;
+    private String date;
+    private int hours;
+
+    public TimeEntry() {
+    }
+
+    public TimeEntry(long projectId, long userId, String date, int hours) {
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
+    }
+
+    public TimeEntry(long id, long projectId, long userId, String date, int hours) {
+        this.id = id;
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TimeEntry timeEntry = (TimeEntry) o;
+
+        if (id != timeEntry.id) return false;
+        if (projectId != timeEntry.projectId) return false;
+        if (userId != timeEntry.userId) return false;
+        if (hours != timeEntry.hours) return false;
+        return date != null ? date.equals(timeEntry.date) : timeEntry.date == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (int) (projectId ^ (projectId >>> 32));
+        result = 31 * result + (int) (userId ^ (userId >>> 32));
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + hours;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeEntry{" +
+            "id=" + id +
+            ", projectId=" + projectId +
+            ", userId=" + userId +
+            ", date='" + date + '\'' +
+            ", hours=" + hours +
+            '}';
+    }
+}

student02-pal/pal-tracker

src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -1,0 +1,88 @@
+package io.pivotal.pal.tracker;
+
+import java.time.LocalDate;
+
+public class TimeEntry {
+    private long id;
+    private long projectId;
+    private long userId;
+    private LocalDate date;
+    private int hours;
+
+    public TimeEntry() {
+    }
+
+    public TimeEntry(long projectId, long userId, LocalDate date, int hours) {
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
+    }
+
+    public TimeEntry(long id, long projectId, long userId, LocalDate date, int hours) {
+        this.id = id;
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TimeEntry timeEntry = (TimeEntry) o;
+
+        if (id != timeEntry.id) return false;
+        if (projectId != timeEntry.projectId) return false;
+        if (userId != timeEntry.userId) return false;
+        if (hours != timeEntry.hours) return false;
+        return date != null ? date.equals(timeEntry.date) : timeEntry.date == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (int) (projectId ^ (projectId >>> 32));
+        result = 31 * result + (int) (userId ^ (userId >>> 32));
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + hours;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeEntry{" +
+            "id=" + id +
+            ", projectId=" + projectId +
+            ", userId=" + userId +
+            ", date='" + date + '\'' +
+            ", hours=" + hours +
+            '}';
+    }
+}